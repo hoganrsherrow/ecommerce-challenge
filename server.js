367d7a8665cf56f5d3051971cba4ae0b52cd1f50
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-09-10 11:33:18 -0500

    added .gitignore and fixed npm vulnerabilities

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
